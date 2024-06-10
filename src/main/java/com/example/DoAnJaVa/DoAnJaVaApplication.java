f4b762c08acec305d93c09c8e3650de07d621aa0
phương uyển lê <lephuonguyen21082003@gmail.com>
2024-06-10 15:03:30 +0700

    layout

---
+++ src/main/java/com/example/DoAnJaVa/DoAnJaVaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoAnJaVa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnJaVaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnJaVaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoAnJaVa/DoAnJaVaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoAnJaVa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnJaVaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
