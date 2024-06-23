d43e681b5e594044f5e900cc391341d0ed5e67a9
oneraix <96623306+oneraix@users.noreply.github.com>
2024-06-23 16:53:38 +0700

    Revert "Hieu"

--- src/main/java/com/example/DoAnJaVa/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.DoAnJaVa.repository;
-import com.example.DoAnJaVa.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
